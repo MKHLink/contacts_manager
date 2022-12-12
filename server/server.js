7b03a337b2431649f983243086ffe7aa980f74f4
MKHLink <[likhonhasankomol@gmail.com]>
2022-12-12 15:41:37 -0500

    made server side files and added scripts to root package

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT||3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('../client/'));
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT,function(){
+    console.log(`Listeneing to port ${PORT}`);
+});
