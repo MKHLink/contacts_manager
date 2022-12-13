5c02330a4e08958e21799ee98c119c8f8620b766
MKHLink <[likhonhasankomol@gmail.com]>
2022-12-13 16:01:36 -0500

    implemented CRUD functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -75,3 +75,18 @@ export const postDb = async (name, email, phone, profile) => {
     console.log('result.value', result);
     return result?.value;
   };
+
+  // EXPORTED EDIT function
+export const editDb = async (id, name, email, phone, profile) => {
+    console.log('PUT to the database');
+  
+    const contactDb = await openDB('contact_db', 1);
+  
+    const tx = contactDb.transaction('contacts', 'readwrite');
+  
+    const store = tx.objectStore('contacts');
+  
+    const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { fetchCards } from "./cards";
 
-import {initdb,getDb,postDb, deleteDb} from './database';
+import {initdb,getDb,postDb, deleteDb, editDb} from './database';
 import { toggleForm, clearForm } from "./form";
 
 window.addEventListener('load',function(){
@@ -44,6 +44,14 @@ window.addEventListener('load',function(){
     postDb(name, email, phone, profile);
   } else {
   
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editDb(profileId, name, email, phone, profile);    
+
     fetchCards();
       // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -64,4 +72,23 @@ window.addEventListener('load',function(){
     deleteDb(id);
     // Reload the DOM
     fetchCards();
+  };
+
+  window.editCard = (e) => {
+    // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+    profileId = parseInt(e.dataset.id);
+  
+    // Grabs information to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+  
+    document.getElementById("name").value = editName;
+    document.getElementById("email").value = editEmail;
+    document.getElementById("phone").value = editPhone;
+  
+    form.style.display = "block";
+  
+    // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+      submitBtnToUpdate = true;
   };
